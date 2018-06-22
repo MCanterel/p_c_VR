1b7adb4b3fca99abb57352ad765386c5aa8bbc4c
MCanterel <nelsnelsnels@gmail.com>
2018-06-22 17:33:52 -0400

    initial commit

---
+++ jnc_helpers/start.js
@@ -0,0 +1,35 @@
+// init info and globals source code
+
+			var clock = new THREE.Clock();
+
+			var container;
+			var camera, scene, raycaster, renderer;
+
+			var room;
+			var isMouseDown = false;
+
+			var INTERSECTED;
+			var crosshair;
+
+
+
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				var info = document.createElement( 'div' );
+				info.style.position = 'absolute';
+				info.style.top = '10px';
+				info.style.width = '100%';
+				info.style.textAlign = 'center';
+				info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> webgl - interactive cubes';
+				container.appendChild( info );
+
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0x505050 );
+
+				room = new THREE.Mesh(
+					new THREE.BoxBufferGeometry( 10, 10, 10, 8, 8, 8 ),
+					new THREE.MeshBasicMaterial( { color: 0x404040, wireframe: true } )
+				);
+				room.position.y = 3;
+				scene.add( room );
